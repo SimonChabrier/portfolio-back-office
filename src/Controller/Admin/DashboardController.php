4f6f8ce7c8d279c64845455b23e67cc8daedf79b
Simon Chabrier <simonchabrier@gmail.com>
2022-10-31 17:31:06 +0100

    finalize user crud first version

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,21 +4,26 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
     /**
+     * @IsGranted("ROLE_ADMIN")
      * @Route("/admin", name="app_admin")
      */
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        return parent::index();
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+        //return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -28,9 +33,15 @@ class DashboardController extends AbstractDashboardController
     }
 
     public function configureMenuItems(): iterable
-    {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+    {   
+        yield MenuItem::linkToLogout('Logout', 'fa fa-fw fa-sign-out');
+        yield MenuItem::section('app admin');
+        //uncomment this line to main Dashboard
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class)->setPermission('ROLE_ADMIN');
+        yield MenuItem::section('link');
+        yield MenuItem::linkToUrl('Portfolio', 'fas fa-link', 'https://simonchabrier.github.io');
+        yield MenuItem::linkToUrl('EasyAdmin', 'fas fa-link', 'https://symfony.com/bundles/EasyAdminBundle/current/index.html');
         //yield MenuItem::linkToCrud('Projects', 'fa fa-comment', Project::class),
     }
 }
