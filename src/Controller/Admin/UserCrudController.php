1a5a5d778271acafae111861ac543c251bf7c5ee
Simon Chabrier <simonchabrier@gmail.com>
2022-10-31 15:27:02 +0100

    user crud V1

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +16,17 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            
+            TextField::new('username')->setLabel('Nom d\'utilisateur'),
+            TextField::new('email')->setLabel('Email'),
+            TextField::new('password')->hideOnIndex()->setLabel('Password'),
+            BooleanField::new('isVerified')->renderAsSwitch(true)->setLabel('Mail Vérifié'),
+            BooleanField::new('status')->renderAsSwitch(true)->setLabel('Statut'),
+            ChoiceField::new('roles')->allowMultipleChoices(true)->setChoices(['Admin' => 'ROLE_ADMIN']),
         ];
     }
-    */
+
 }
