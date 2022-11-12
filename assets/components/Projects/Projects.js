1c44faf38755cc7c3a35c3cbfe4e3cd07ba03d08
Simon Chabrier <simonchabrier@gmail.com>
2022-11-12 20:04:55 +0100

    add projects v2

--- assets/components/Projects/Projects.js
+++ assets/components/Projects/Projects.js
@@ -1,21 +1,30 @@
 import './style.scss';
 import React from "react";
-import Project from './Project';
 
 const Projects = ({ projects }) => (
-
-        projects.map((project) => (
-            <Project 
-                title = {project.title}
-                description = {project.desciption}
-                image = {project.picture}
-                links = {project.links}
-                id = {project.id}
-            />
-     
+    
+        projects.map((project, index) => (
+            <div className="project" key={index}>
+                <h1>{project.title}</h1>
+                <p>{project.id}</p>
+                <img src={ `media/cache/thumb/assets/media/${project.picture}` } alt={project.title} />
+                <p>{project.desciption}</p>
+                <ul>
+                    {project.links.map((link, index) => (
+                        <li key={index}>
+                        <a className="project--links" href={link.link}> {link.link}</a>
+                        </li>
+                    ))}
+                </ul>
+                <ul>
+                    {project.technos.map((techno, index) => (
+                        <li key={index}>
+                        {techno.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         ))
-
-
 );
 
 
