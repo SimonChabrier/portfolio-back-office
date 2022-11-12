1fe550c356227576c07cb9981f644a61245c8d3a
Simon Chabrier <simonchabrier@gmail.com>
2022-11-12 20:18:22 +0100

    add cards and scss

--- assets/components/Projects/Projects.js
+++ assets/components/Projects/Projects.js
@@ -4,29 +4,36 @@ import React from "react";
 const Projects = ({ projects }) => (
     
         projects.map((project, index) => (
-            <div className="project" key={index}>
-                <h1>{project.title}</h1>
-                <p>{project.id}</p>
+            <div className="card" id="card" key={index}>
+            <div className="card--header">
+                <h2 className="card--title" id="title">{project.title}</h2>
+                <div className="card--picture" id="picture">
                 <img src={ `media/cache/thumb/assets/media/${project.picture}` } alt={project.title} />
-                <p>{project.desciption}</p>
-                <ul>
-                    {project.links.map((link, index) => (
-                        <li key={index}>
-                        <a className="project--links" href={link.link}> {link.link}</a>
-                        </li>
-                    ))}
-                </ul>
-                <ul>
-                    {project.technos.map((techno, index) => (
-                        <li key={index}>
-                        {techno.name}
-                        </li>
-                    ))}
-                </ul>
+                </div>
+                <div className="card--date">
+                    <span id="date">{project.date}</span>
+                </div>
             </div>
+            <div className="card--content">
+                <div className="card--techno" id="techno">
+                {project.technos.map((techno, index) => (
+                    <span className="card--span" id="span" key={index}>{techno.name}</span>
+                ))}
+                </div>
+                <div className="card--text">
+                    <p id="text">{project.desciption}</p>
+                </div>
+            </div>
+            <div className="card--footer" id="web">
+                {project.links.map((link, index) => (
+                    <a className="project--links" href={link.link} key={index}> {link.link}</a>
+                ))}
+            </div>
+        </div>
         ))
 );
 
+                
 
 
 export default Projects;
